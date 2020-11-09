6e24d70373369b2cfff04c9e8018f7c2a74b3f50
mclusky <chrismaryme@yahoo.co.uk>
2020-11-09 14:43:45 +0100

    Update mongoose.js

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb+srv://taskapp:Store*_*49@cluster0-bkulv.mongodb.net/kit??retryWrites=true", {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-//   })
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch(err => console.log(err));
-
 mongoose.connect(process.env.DATABASE, {
         useNewUrlParser: true,
         useCreateIndex: true,
